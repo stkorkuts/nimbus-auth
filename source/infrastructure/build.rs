2d7d792e17a34279c150bf406c88e23b26bfacf7
Stanislau Korkuts <stkorkuts@gmail.com>
2025-09-01 17:18:50 +0300

    Defined initial application project structure

---
+++ source/infrastructure/build.rs
@@ -0,0 +1,12 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut config = prost_build::Config::new();
+    config.include_file("proto.rs").compile_protos(
+        &[
+            "../../proto/v1/auth/signup.proto",
+            "../../proto/v1/auth/signin.proto",
+            "../../proto/v1/auth/refresh.proto",
+        ],
+        &["../../proto"],
+    )?;
+    Ok(())
+}
